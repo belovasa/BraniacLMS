529f37bc5579d198d347cb34280a524a7e1bf14d
belovasa <belosa2604@yandex.ru>
2022-11-12 21:53:48 +0400

    lesson_2 Static site with Django

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,4 @@
 
-
 from django.urls import path
 
 from mainapp import views
@@ -8,6 +7,10 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path("", views.HelloWorldView.as_view()),
-    path("<str:word>/", views.check_kwargs),
+    path("", views.MainPageView.as_view()),
+    path("news/", views.NewsPageView.as_view()),
+    path("courses/", views.CoursesPageView.as_view()),
+    path("contacts/", views.ContactsPageView.as_view()),
+    path("doc_site/", views.DocSitePageView.as_view()),
+    path("login/", views.LoginPageView.as_view()),
 ]
